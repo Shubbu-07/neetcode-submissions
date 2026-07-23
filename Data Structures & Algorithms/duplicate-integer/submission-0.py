4a52319617da6f33ba14f0d7e508014dfc5b2902
Shubham Mergu <95517382+Shubbu-07@users.noreply.github.com>
2026-07-23 17:31:49 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            seen.add(num)
+        
+        if len(nums) != len(seen):
+            return True
+        else:
+            return False
---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq_s = {}
+        freq_t = {}
+
+        for ch in s:
+            if ch in freq_s:
+                freq_s[ch] +=1
+            else:
+                freq_s[ch] = 1
+
+        for ch in t:
+            if ch in freq_t:
+                freq_t[ch] +=1
+            else:
+                freq_t[ch] = 1
+
+        if freq_s == freq_t:
+            return True
+        else:
+            return False
